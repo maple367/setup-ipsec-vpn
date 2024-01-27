f344d55ce04801c725da3ae230dda60c9b986a53
Hai HUANG <101720804+maple367@users.noreply.github.com>
2024-01-27 14:43:50 +0800

    Update vpnsetup_alpine.sh

--- vpnsetup_alpine.sh
+++ vpnsetup_alpine.sh
@@ -70,7 +70,7 @@ check_os() {
       ;;
   esac
   os_ver=$(. /etc/os-release && printf '%s' "$VERSION_ID" | cut -d '.' -f 1,2)
-  if [ "$os_ver" != "3.17" ] && [ "$os_ver" != "3.18" ]; then
+  if [ "$os_ver" != "3.17" ] && [ "$os_ver" != "3.18" ] && [ "$os_ver" != "3.19" ]; then
     exiterr "This script only supports Alpine Linux 3.17/3.18."
   fi
 }
